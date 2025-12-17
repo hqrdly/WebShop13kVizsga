73a453f50f78c2826bf4485f263671be092d04bf
mizsei.matyas <mizsei.matyas@C113-05>
2025-12-17 11:20:10 +0100

    CategoryModel

--- WebShop13kVizsga/Program.cs
+++ WebShop13kVizsga/Program.cs
@@ -1,12 +1,42 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using WebShop13kVizsga.Model;
+using WebShop13kVizsga.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContextPool<DataDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("connect")));
+builder.Services.AddTransient<CategoryModel>();
+builder.Services.AddTransient<ItemModel>();
+builder.Services.AddTransient<OrderModel>();
+builder.Services.AddTransient<UserModel>();
+builder.Services.AddTransient<WorkerModel>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
+    options.LoginPath = "/api/User/login";
+    options.LogoutPath = "/api/User/logout";
+    options.Events = new CookieAuthenticationEvents
+    {
+        OnRedirectToLogin = ctx =>
+        {
+            ctx.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        },
+        OnRedirectToAccessDenied = ctx =>
+        {
+            ctx.Response.StatusCode = 403;
+            return Task.CompletedTask;
+        }
+    };
+});
+
+//builder.Services.AddAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
