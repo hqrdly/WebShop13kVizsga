26dd6ace228ae56c23fede8436570119e35f2c00
mizsematyas <mizse@BEAST_PC>
2026-01-08 18:01:31 +0100

    CartModel, CartController

--- WebShop13kVizsga/Controllers/CartsController.cs
+++ WebShop13kVizsga/Controllers/CartsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebShop13kVizsga.Dto;
 using WebShop13kVizsga.Model;
 
 namespace WebShop13kVizsga.Controllers
@@ -14,6 +15,74 @@ namespace WebShop13kVizsga.Controllers
             _model = model;
         }
 
+        [HttpGet("/get-cart-byid/{cartId:int}")]
+        public ActionResult<IEnumerable<CartDto>> GetCart([FromRoute] int cartId)
+        {
+            try
+            {
+                return Ok(_model.GetCart(cartId));
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
+        [HttpGet("/total-price")]
+        public ActionResult<int> GetCartTotalPrice([FromQuery] int cartId)
+        {
+            try
+            {
+                return Ok(_model.GetCartTotalPrice(cartId));
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("/modify-cart/{cartId:int}")]
+        public ActionResult ModifyCart(
+            [FromRoute] int cartId,
+            [FromBody] ModifyCartDto dto)
+        {
+            try
+            {
+                _model.ModifyCart(cartId, dto);
+                return Ok();
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("/delete-item-from-cart/{id:int}")]
+        public ActionResult DeleteItemFromCart([FromRoute] int id)
+        {
+            try
+            {
+                _model.DeleteItemFromCart(id);
+                return Ok();
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("/modify-item-quantity/{id:int}")]
+        public ActionResult ModifyItemQuantity([FromQuery] int id, [FromQuery] int quantity)
+        {
+            try
+            {
+                _model.ModifyItemQuantity(id, quantity);
+                return Ok();
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- WebShop13kVizsga/Model/CartModel.cs
+++ WebShop13kVizsga/Model/CartModel.cs
@@ -14,22 +14,120 @@ namespace WebShop13kVizsga.Model
 
 
         #region CartInventory
+        public IEnumerable<CartDto> GetCart(int id)
+        {
+            if (!_context.Carts.Any(x => x.CartId == id))
+            {
+                throw new InvalidOperationException("Nincs ilyen kosár");
+            }
+
+            return _context.Carts
+                .Where(x => x.CartId == id)
+                .Select(x => new CartDto
+                {
+                    itemName = x.ItemName,
+                    price = x.Price,
+                    quantity = x.Quantity
+                })
+                .ToList();
+        }
         #endregion
 
-
         #region CartTotalPrice
+        public int GetCartTotalPrice(int id)
+        {
+            if (!_context.Carts.Any(x => x.CartId == id))
+            {
+                throw new InvalidOperationException("Nincs ilyen kosár");
+            }
+            return _context.Carts
+                .Where(x => x.CartId == id)
+                .Sum(x => x.Price * x.Quantity);
+        }
         #endregion
 
         #region ModifyCart, (ModifyCartTotalPrice)
+        public void ModifyCart(int id, ModifyCartDto dto)
+        {
+            if (!_context.Carts.Any(x => x.CartId == id))
+            {
+                throw new InvalidOperationException("Nincs ilyen kosár");
+            }
+            else
+            {
+                using var trx = _context.Database.BeginTransaction();
+                try
+                {
+                    var cart = _context.Carts.First(x => x.CartId == id);
+                    cart.ItemName = dto.Modif_ItemName;
+                    cart.Price = dto.Modif_Price;
+                    cart.Quantity = dto.Modif_Quantity;
+                    _context.SaveChanges();
+                    trx.Commit();
+                }
+                catch
+                {
+                    trx.Rollback();
+                    throw;
+                }
+            } 
+        }
         #endregion
 
         #region ModifyCartStatus
+
         #endregion
 
         #region ModifyItemQuantity
+        public void ModifyItemQuantity(int id, int quantity)
+        {
+            if (!_context.Carts.Any(x => x.CartId == id))
+            {
+                throw new InvalidOperationException("Nincs ilyen kosár");
+            }
+            else
+            {
+                using var trx = _context.Database.BeginTransaction();
+                try
+                {
+                    var cart = _context.Carts.First(x => x.CartId == id);
+                    cart.Quantity = quantity;
+                    _context.SaveChanges();
+                    trx.Commit();
+                }
+                catch
+                {
+                    trx.Rollback();
+                    throw;
+                }
+            }
+        }
         #endregion
 
-        #region 
+        #region DeleteItemFromCart
+        public void DeleteItemFromCart(int id)
+        {
+            if (!_context.Carts.Any(x => x.CartId == id))
+            {
+                throw new InvalidOperationException("Nincs ilyen kosár");
+            }
+            else
+            {
+                using var trx = _context.Database.BeginTransaction();
+                try
+                {
+                    var cart = _context.Carts.First(x => x.CartId == id);
+                    _context.Carts.Remove(cart);
+                    _context.SaveChanges();
+                    trx.Commit();
+                }
+                catch
+                {
+                    trx.Rollback();
+                    throw;
+                }
+            }
+        }
         #endregion
     }
 }
